2454f02bc706b796672b239b2f5dff5094f56e74
etissieres <etienne.tissieres@gmail.com>
2014-09-26 20:36:22 +0200

    system services

--- app.js
+++ app.js
@@ -1,7 +1,22 @@
+var _ = require('lodash');
+
+
 var logmore = require('logmore');
 logmore.use(require('logmore-console')());
 var logger = logmore('monux');
 logmore.enable('monux', 'info');
+
+
+var system = require('./core/services/system');
+
+var wrappers = require('./core/wrappers');
+wrappers.createStaticService('kernel', system.kernel);
+wrappers.createStaticService('cpu', system.cpu);
+wrappers.createStaticService('mem', system.mem);
+wrappers.createOnDemandService('disk', system.disk);
+
+var staticData = null;
+
 
 var express = require('express');
 var app = express();
@@ -14,11 +29,19 @@ nunjucks.configure('web', {
   express: app
 });
 
-app.use(express.static(__dirname + '/web'));
-
 app.get('/', function (req, res) {
-  res.render('index.html');
+  wrappers.collectOnDemandServicesData(function (onDemandData) {
+    var data = _.clone(staticData);
+    _.assign(data, onDemandData);
+    res.render('index.html', data);
+  });
 });
 
-server.listen(1337);
-logger.info('Server running at *:1337');
+app.use(express.static(__dirname + '/web'));
+
+
+wrappers.collectStaticServicesData(function (data) {
+  staticData = data;
+  server.listen(1337);
+  logger.info('Server running at *:1337');
+});
